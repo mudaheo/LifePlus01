efe58ab9f829c13f5889570b5324bb4810a1c731
mudaheo <mudaheo@gmail.com>
2016-12-26 15:50:47 +0700

    Create homePage branch

---
+++ LifePlus/HomePageViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageViewController.swift
+//  LifePlus
+//
+//  Created by Javu on 12/26/16.
+//  Copyright © 2016 Javu. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
