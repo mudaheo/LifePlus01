80ec0874559ae13ee154b6007754fcc839ca055e
Nhan Phung <nhanpb77@gmail.com>
2017-01-10 10:44:59 +0700

    Update UI - Add Login

---
+++ LifePlus/ForgotPassViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgotPassViewController.swift
+//  LifePlus
+//
+//  Created by Nhân Phùng on 1/10/17.
+//  Copyright © 2017 Javu. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
